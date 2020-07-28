3d051892b9ea7cd837b24bbae10fe79e1aa10353
Stephen Miracle <stephen@specbooks.com>
2020-07-27 20:43:13 -0400

    adding authentication content.

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,13 +8,19 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
 
 import Header from "./header"
-import "./layout.css"
+import "./layout.css";
+
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+};
 
 const Layout = ({ children }) => {
-  console.log(process.env.GATSBY_SAMPLE_APP);
-  console.log(process.env.GATSBY_APP_SAMPLE);
+
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -25,8 +31,15 @@ const Layout = ({ children }) => {
     }
   `)
 
+  const {user, isAuthenticated} = useAuth0();
+
   return (
     <>
+      <Auth0Provider
+      domain="projectreports.us.auth0.com"
+      clientId="mtzNRkzuiCL0wNSL9S7SCv2CXDxU0jHH"
+      redirectUri={'http://localhost:8000/callback'}
+    >
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
@@ -35,13 +48,17 @@ const Layout = ({ children }) => {
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
-        <main>{children}</main>
+        <main>
+          <LoginButton />
+        {children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">{process.env.GATSBY_SAMPLE_APP} Gatsby {process.env.GATSBY_APP_SAMPLE} </a>
         </footer>
       </div>
+    </Auth0Provider>
+      
     </>
   )
 }
---
+++ src/pages/callback.tsx
@@ -0,0 +1,14 @@
+
+import React from "react"
+import Layout from "../components/layout";
+
+
+export default function Callback() {
+    return (
+        <>
+            <Layout>
+                <h1>Say What</h1>
+            </Layout>
+        </>
+    )
+}
