15b00fcf6409964459a00086a021b1d23ea6cc2e
Adam Alicki <aalicki@gmail.com>
2020-07-31 15:55:51 -0400

    Adding in Stripe checkout Express form.

---
+++ src/pages/checkout.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect } from "react"
+
+import Layout from "../components/layout/layout"
+import SEO from "../components/seo"
+import { useAuth0 } from "@auth0/auth0-react"
+
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "../components/checkout/CheckoutForm";
+
+import { Container, Row, Col } from 'react-bootstrap';
+import * as process from "process";
+
+const stripePromise = loadStripe(process.env.GATSBY_STRIPE_SECRET_KEY);
+
+const redirectToCheckout = async event => {
+
+    event.preventDefault()
+
+    const stripe = await stripePromise
+
+    const { error } = await stripe.redirectToCheckout({
+        items: [{ sku: process.env.GATSBY_BUTTON_SKU_ID, quantity: 1 }],
+        successUrl: `${window.location.origin}/page-2/`,
+        cancelUrl: `${window.location.origin}/`,
+    })
+
+    if (error) {
+        console.warn("Error:", error)
+    }
+}
+
+const Checkout = () => {
+    const { user, isAuthenticated } = useAuth0();
+    return (
+        <Layout pageName="Home" user={user} isAuthenticated={isAuthenticated}>
+            <SEO title="Home" keywords={[`gatsby`, `react`, `bootstrap`]} />
+            <Container>
+                <Row>
+                    <Col>
+                        <div className="App">
+                            <Elements stripe={stripePromise}>
+                                <CheckoutForm />
+                            </Elements>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </Layout>
+    )
+}
+
+export default Checkout
