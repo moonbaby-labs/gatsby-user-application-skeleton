3daa468f41b0e4b9ea7fe202fcf8cb8d8a82d529
stephen miracle <miracle@stephens-MBP.attlocal.net>
2020-08-03 01:26:40 -0400

    adding variables terraform file.

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "GCP_SERVICE_KEY" {
+  type = string
+}
+
+variable "PROJECT_NAME" {
+  type = string
+}
+
+variable "REGION" {
+  type = string
+}
+variable "BUCKET_NAME" {
+  type = string
+}
