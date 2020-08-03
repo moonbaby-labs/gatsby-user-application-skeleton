ade04cc11ceba88e6394d1d0222922bfe9a55a42
stephen miracle <miracle@stephens-MBP.attlocal.net>
2020-08-03 10:04:47 -0400

    updating terraform setup.

--- terraform/gcp_main.tf
+++ terraform/gcp_main.tf
@@ -4,100 +4,142 @@ provider "google" {
     region      = "${var.REGION}"
 }
 
-resource "google_storage_bucket" "static" {
-  name          = "${var.BUCKET_NAME}"
-  location      = "${var.REGION}"
+# ------------------------------------------------------------------------------
+# PREPARE LOCALS
+#
+# NOTE: Due to limitations in terraform and heavy use of nested sub-blocks in the resource,
+# we have to construct some of the configuration values dynamically
+# ------------------------------------------------------------------------------
 
-  bucket_policy_only = true
-  
-
-  website {
-    main_page_suffix = "index.html"
-    not_found_page   = "404.html"
-  }
-  cors {
-    origin          = ["${var.BUCKET_NAME}"]
-    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
-    response_header = ["*"]
-    max_age_seconds = 3600
-  }
+locals {
+  # We have to use dashes instead of dots in the access log bucket, because that bucket is not a website
+  website_domain_name_dashed = replace(var.website_domain_name, ".", "-")
+  access_log_kms_keys        = var.access_logs_kms_key_name == "" ? [] : [var.access_logs_kms_key_name]
+  website_kms_keys           = var.website_kms_key_name == "" ? [] : [var.website_kms_key_name]
 }
 
-resource "google_storage_bucket_iam_member" "member" {
-  bucket = "${var.BUCKET_NAME}"
-  role = "roles/storage.objectViewer"
-  member = "allUsers"
-}
+# ------------------------------------------------------------------------------
+# CREATE THE WEBSITE BUCKET
+# ------------------------------------------------------------------------------
 
-resource "google_compute_backend_bucket" "site_backend" {
-  name        = "${var.BUCKET_NAME}"
-  bucket_name = "${var.BUCKET_NAME}"
-  enable_cdn  = true
-}
-
-resource "google_compute_url_map" "urlmap" {
-  name        = "urlmap"
-  description = "a description"
-  default_service = google_compute_backend_service.home.id
-}
-
-resource "google_compute_backend_service" "home" {
-  name        = "home"
-  port_name   = "http"
-  protocol    = "HTTP"
-  timeout_sec = 10
-
-  health_checks = [google_compute_health_check.default.id]
-  load_balancing_scheme = "INTERNAL_SELF_MANAGED"
-}
-
-resource "google_compute_health_check" "default" {
-  name               = "health-check"
-  http_health_check {
-    port = 80
-  }
-}
-
-
-resource "google_compute_global_forwarding_rule" "https" {
-  provider   = google-beta
-  project    = var.PROJECT_NAME
-  count      = 1
-  name       = "${var.BUCKET_NAME}"
-  target     = google_compute_target_https_proxy.default[0].self_link
-  port_range = "443"
-}
-
-
-resource "google_compute_managed_ssl_certificate" "default" {
+resource "google_storage_bucket" "website" {
   provider = google-beta
 
-  name = "test-cert"
+  project = var.project
 
-  managed {
-    domains = ["${var.APP_DOMAIN}."]
+  name          = var.website_domain_name
+  location      = var.website_location
+  storage_class = var.website_storage_class
+
+  versioning {
+    enabled = var.enable_versioning
+  }
+
+  website {
+    main_page_suffix = var.index_page
+    not_found_page   = var.not_found_page
+  }
+
+  dynamic "cors" {
+    for_each = var.enable_cors ? ["cors"] : []
+    content {
+      origin          = var.cors_origins
+      method          = var.cors_methods
+      response_header = var.cors_extra_headers
+      max_age_seconds = var.cors_max_age_seconds
+    }
+  }
+
+  force_destroy = var.force_destroy_website
+
+  dynamic "encryption" {
+    for_each = local.website_kms_keys
+    content {
+      default_kms_key_name = encryption.value
+    }
+  }
+
+  labels = var.custom_labels
+  logging {
+    log_bucket        = google_storage_bucket.access_logs.name
+    log_object_prefix = var.access_log_prefix != "" ? var.access_log_prefix : local.website_domain_name_dashed
   }
 }
 
+# ------------------------------------------------------------------------------
+# CONFIGURE BUCKET ACLS
+# ------------------------------------------------------------------------------
 
-resource "google_compute_target_https_proxy" "default" {
-  project = var.PROJECT_NAME
-  count   = 1
-  name    = "${var.BUCKET_NAME}"
-  url_map = google_compute_url_map.urlmap.id
-
-  ssl_certificates = [google_compute_managed_ssl_certificate.default.id]
+resource "google_storage_default_object_acl" "website_acl" {
+  provider    = google-beta
+  bucket      = google_storage_bucket.website.name
+  role_entity = var.website_acls
 }
 
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE A SEPARATE BUCKET TO STORE ACCESS LOGS
+# ---------------------------------------------------------------------------------------------------------------------
 
-resource "google_dns_record_set" "dns" {
-  project = var.PROJECT_NAME
-  count   = 1
+resource "google_storage_bucket" "access_logs" {
+  provider = google-beta
 
-  name = "${var.APP_DOMAIN}."
-  type = "CNAME"
-  ttl  = 300
-  rrdatas = [google_compute_backend_bucket.site_backend.self_link]
+  project = var.project
 
-  managed_zone = var.DNS_ZONE_NAME
+  # Use the dashed domain name
+  name          = "${local.website_domain_name_dashed}-logs"
+  location      = var.website_location
+  storage_class = var.website_storage_class
+
+  force_destroy = var.force_destroy_access_logs_bucket
+
+  dynamic "encryption" {
+    for_each = local.access_log_kms_keys
+    content {
+      default_kms_key_name = encryption.value
+    }
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+
+    condition {
+      age = var.access_logs_expiration_time_in_days
+    }
+  }
+  labels = var.custom_labels
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# GRANT WRITER ACCESS TO GOOGLE ANALYTICS
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "google_storage_bucket_acl" "analytics_write" {
+  provider = google-beta
+
+  bucket = google_storage_bucket.access_logs.name
+
+  # The actual identity is 'cloud-storage-analytics@google.com', but
+  # we're required to prefix that with the type of identity
+  role_entity = ["WRITER:group-cloud-storage-analytics@google.com"]
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE OPTIONAL CNAME ENTRY IN CLOUD DNS
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "google_dns_record_set" "cname" {
+  provider = google-beta
+  count    = var.create_dns_entry ? 1 : 0
+
+  depends_on = [google_storage_bucket.website]
+
+  project = var.project
+
+  name         = "${var.website_domain_name}."
+  managed_zone = var.dns_managed_zone_name
+  type         = "CNAME"
+  ttl          = var.dns_record_ttl
+  rrdatas      = ["c.storage.googleapis.com."]
 }
